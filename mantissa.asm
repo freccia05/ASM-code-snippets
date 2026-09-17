8eda8790cfc2dc28a3864e7dd9e89d8848ded6aa
fremi <fremi@nonexistent.com>
2026-09-17 16:22:27 -0300

    WHKERJVBWR

---
+++ mantissa.asm
@@ -0,0 +1,40 @@
+(store four test bytes into $0200-$0203)
+
+LDA #$00
+STA $0200
+
+LDA #$00
+STA $0201
+
+LDA #$00
+STA $0202
+
+LDA #$3F
+STA $0203
+
+
+JSR getMantissa
+
+LDA $0210     ; A = 00
+LDA $0211     ; A = 00
+LDA $0212     ; A = BF
+
+BRK
+
+getMantissa:
+
+;loop and copy
+LDX #$00
+LOOP:
+LDA $0201,X   ; read source byte
+STA $0210,X   ; write it one slot lower in the result
+INX
+CPX #$03
+BNE LOOP
+
+;OR
+LDA $0212
+ORA #$80
+STA $0212
+
+RTS
